b65b4d4767f83520076dd7f1800c272bb397250b
Maria Gabriela <mariagabrielarpolicarpo@hotmail.com>
2022-07-20 11:40:42 -0300

    last commit

--- DesafiodeProjeto/Main.java
+++ DesafiodeProjeto/Main.java
@@ -12,16 +12,45 @@ public class Main {
 
         Curso curso2 = new Curso(); 
         curso2.setCargaHoraria(4); 
-        curso2.setDescricao("descrição curso js"); 
-        curso2.setTitulo("js"); 
+        curso2.setDescricao("HTML CSS E JS"); 
+        curso2.setTitulo("JavaScript"); 
 
         Mentoria mentoria1 = new Mentoria(); 
         mentoria1.setTitulo("mentoria de java"); 
         mentoria1.setDescricao("descrição da mentoria Java"); 
         mentoria1.setData(LocalDate.now()); 
 
-        System.out.println(curso1); 
-        System.out.println(curso2); 
-        System.out.println(mentoria1);
+        Bootcamp bootcamp1 = new Bootcamp(); 
+        bootcamp1.setNome("Santander Code Girls"); 
+        bootcamp1.setDescricao("Java e Spring Framework");
+        bootcamp1.getConteudos().add(curso1);  
+        bootcamp1.getConteudos().add(mentoria1);
+
+        Bootcamp bootcamp2 = new Bootcamp(); 
+        bootcamp2.setNome("JavaScript"); 
+        bootcamp2.setDescricao("HTML CSS E JS"); 
+        bootcamp2.getConteudos().add(curso2); 
+
+        Dev devMariaGabriela = new Dev(); 
+        devMariaGabriela.setNome("Maria Gabriela Rodrigues Policarpo"); 
+        devMariaGabriela.inscreverBootcamp(bootcamp1);
+        System.out.println("Maria Inscrita: " + devMariaGabriela.getConteudosInscritos()); 
+        devMariaGabriela.progredir();
+        devMariaGabriela.progredir(); 
+        System.out.println("Maria depois de progredir: " + devMariaGabriela.getConteudosInscritos());
+        System.out.println("O que Maria concluiu: " + devMariaGabriela.getConteudosConcluidos());
+        System.out.println("XP após concluir: " + devMariaGabriela.calcularTotalXp()); 
+
+        System.out.println("--------------"); 
+
+        Dev devCamila = new Dev(); 
+        devCamila.setNome("Camila Cavalcanti");
+        devCamila.inscreverBootcamp(bootcamp2); 
+        System.out.println("Camila: " + devCamila.getConteudosInscritos()); 
+        devCamila.progredir();
+        System.out.println("Camila depois de progredir: " + devCamila.getConteudosInscritos());
+        System.out.println("O que Camila concluiu: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP após concluir: " + devCamila.calcularTotalXp());
+
     }
 }
