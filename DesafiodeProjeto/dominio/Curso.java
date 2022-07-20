d9970fcd01a12d6d070aaa06d7ab6e72419c3f61
Maria Gabriela <mariagabrielarpolicarpo@hotmail.com>
2022-07-20 10:51:04 -0300

    commit 3

--- DesafiodeProjeto/dominio/Curso.java
+++ DesafiodeProjeto/dominio/Curso.java
@@ -1,29 +1,23 @@
 package dominio; 
 
-public class Curso {
+public class Curso extends Conteudo {
     
-    private String titulo; 
-    private String descricao; 
+
     private int cargaHoraria; 
+    
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
+    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
     }
-    public String getDescricao() {
-        return descricao;
-    }
-    public String getTitulo() {
-        return titulo;
-    }
+
     public void setCargaHoraria(int cargaHoraria) {
         this.cargaHoraria = cargaHoraria;
     }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+
 
     @Override
     public String toString() {
--- DesafiodeProjeto/dominio/Mentoria.java
+++ DesafiodeProjeto/dominio/Mentoria.java
@@ -2,29 +2,22 @@ package dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
     
-    private String titulo; 
-    private String descricao; 
     private LocalDate data;
+    
+    
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO + 20d;
+    }
 
     public LocalDate getData() {
         return data;
     }
-    public String getDescricao() {
-        return descricao;
-    }
-    public String getTitulo() {
-        return titulo;
-    }
+
     public void setData(LocalDate data) {
         this.data = data;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
     }
 
     @Override
@@ -34,7 +27,7 @@ public class Mentoria {
             ", descricao='" + getDescricao() + "'" +
             ", data='" + getData() + "'" +
             "}";
-    }
+        }
     
     
 }
